dc3bcbd3022de121ae8b36fb9399216a460b33be
wtt1002 <wtt1002@qq.com>
2019-03-15 21:18:29 +0800

    病历详情

--- src/views/nav1/MedicalHistory.vue
+++ src/views/nav1/MedicalHistory.vue
@@ -2,33 +2,55 @@
   <el-tabs type="border-card" style="margin-top:10px">
     <el-tab-pane>
       <span slot="label">
-        <i class="el-icon-date"></i> 我的行程
+        <i class="el-icon-date"></i> 患者病历
       </span>
-	  <HistoryPage></HistoryPage>
+      <HistoryPage></HistoryPage>
     </el-tab-pane>
-    <el-tab-pane label="消息中心">
-		<ExamPage></ExamPage>
+    <el-tab-pane label="入院评估">
+      <AssessmentPage></AssessmentPage>
     </el-tab-pane>
-    <el-tab-pane label="角色管理">入院检查</el-tab-pane>
-    <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+    <el-tab-pane label="入院康复检查项目">
+      <AdmissionCheckPage></AdmissionCheckPage>
+    </el-tab-pane>
+    <el-tab-pane label="辅助检查">
+      <ExamPage></ExamPage>
+    </el-tab-pane>
+    <el-tab-pane label="冠脉介入">
+      <PCIPage></PCIPage>
+    </el-tab-pane>
+    <el-tab-pane label="出院小结">
+      <ConclusionPage></ConclusionPage>
+    </el-tab-pane>
+    <el-tab-pane label="用药方案">用药方案</el-tab-pane>
+    <el-tab-pane label="随访记录">
+      <FollowUpPage></FollowUpPage>
+    </el-tab-pane>
   </el-tabs>
 </template>
 
 <script>
 import HistoryPage from "@/components/mh-pages/HistoryPage.vue";
-import ExamPage from "@/components/mh-pages/ExamPage.vue"
+import ExamPage from "@/components/mh-pages/ExamPage.vue";
+import AdmissionCheckPage from "@/components/mh-pages/AdmissionCheckPage.vue";
+import ConclusionPage from "@/components/mh-pages/ConclusionPage.vue";
+import FollowUpPage from "@/components/mh-pages/FollowUpPage.vue";
+import PCIPage from "@/components/mh-pages/PCIPage.vue";
+import AssessmentPage from "@/components/mh-pages/AssessmentPage.vue";
 export default {
   data() {
-    return {
-    };
+    return {};
   },
 
   components: {
     HistoryPage,
-    ExamPage
+    ExamPage,
+    AdmissionCheckPage,
+    ConclusionPage,
+    FollowUpPage,
+    PCIPage,
+    AssessmentPage
   },
 
-  methods: {
-  }
+  methods: {}
 };
 </script>
